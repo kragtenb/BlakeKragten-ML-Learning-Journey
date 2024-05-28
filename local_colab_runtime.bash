3f39ff9d575833022ebdeeee4d01ef93c219e9d1
Blake Kragten <kragtenb@gmail.com>
2024-05-27 22:16:40 -0700

    Removing unneeded files

--- local_colab_runtime.bash
+++ local_colab_runtime.bash
@@ -1,1 +1,3 @@
-python -m notebook     --NotebookApp.allow_origin='https://colab.research.google.com'     --port=8888     --NotebookApp.port_retries=0
+version https://git-lfs.github.com/spec/v1
+oid sha256:806b8102b76899cddd6a639e3487576a73dbe08108d7730f4273fb4c37a14336
+size 135
